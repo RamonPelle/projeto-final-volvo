4aa464f760e016695c7a1f2d8e2a1911b2f0592e
Ramon Pelle <ramon.pelle15@gmail.com>
2026-02-04 19:12:01 -0300

    [funcionalidade] nova url referenciando o swagger Co-authored-by: ana-borowsky <anavonborowsky@gmail.com>

--- TechStore/Program.cs
+++ TechStore/Program.cs
@@ -7,6 +7,22 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = "techstore";
+    });
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = "swagger";
+    });
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
