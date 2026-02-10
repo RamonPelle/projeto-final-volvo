469948d6a38f1c6fb8249caeb3db9b934aed2de1
ana <114084376+ana-borowsky@users.noreply.github.com>
2026-02-10 07:57:11 -0300

    [refactor][categoria] refatora controller

--- TechStore/Controllers/APIs/CategoriaController.cs
+++ TechStore/Controllers/APIs/CategoriaController.cs
@@ -27,73 +27,32 @@ namespace TechStore.Controllers.api
         public async Task<ActionResult<Categoria>> BuscarCategoriaPorId(int id)
         {
             var categoria = await _categoriaService.BuscarCategoriaPorId(id);
-
-            if (categoria == null)
-                return NotFound();
-
             return Ok(categoria);
         }
 
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> DeletarCategoria(int id)
         {
-            try
-            {
-                await _categoriaService.DeletarCategoria(id);
-                return NoContent();
-            }
-            catch (KeyNotFoundException)
-            {
-                return NotFound();
-            }
-            catch (ArgumentException ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            await _categoriaService.DeletarCategoria(id);
+            return NoContent();
         }
 
         [HttpPost]
         public async Task<ActionResult> AdicionarCategoria([FromBody] CategoriaRequest categoriaRequest)
         {
-            if (!ModelState.IsValid)
-            {
-                var errorMessages = ModelState.Values.SelectMany(v => v.Errors);
-                return BadRequest(errorMessages);
-            }
-
-            try
-            {
-                var novaCategoria = await _categoriaService.AdicionarCategoria(categoriaRequest);
-                return CreatedAtAction(nameof(BuscarCategorias), new { id = novaCategoria.Id }, novaCategoria);
-            }
-            catch (ArgumentException ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var novaCategoria = await _categoriaService.AdicionarCategoria(categoriaRequest);
+            return CreatedAtAction(
+                nameof(BuscarCategorias),
+                new { id = novaCategoria.Id },
+                novaCategoria
+            );
         }
 
         [HttpPut("{id:int}")]
         public async Task<IActionResult> AtualizarCategoria(int id, [FromBody] CategoriaRequest categoriaRequest)
         {
-            if (!ModelState.IsValid)
-            {
-                var errorMessages = ModelState.Values.SelectMany(v => v.Errors);
-                return BadRequest(errorMessages);
-            }
-
-            try
-            {
-                await _categoriaService.AtualizarCategoria(id, categoriaRequest);
-                return NoContent();
-            }
-            catch (KeyNotFoundException)
-            {
-                return NotFound();
-            }
-            catch (ArgumentException ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            await _categoriaService.AtualizarCategoria(id, categoriaRequest);
+            return NoContent();
         }
 
     }
