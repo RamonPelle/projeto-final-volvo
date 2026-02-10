e05347deb81d1dce4b3dd2403e1e2d9de29ec0e6
ana <114084376+ana-borowsky@users.noreply.github.com>
2026-02-10 13:19:01 -0300

    [ajuste] remove código desnecessário e altera casas de decimal

--- TechStore/Models/ItemPedido.cs
+++ TechStore/Models/ItemPedido.cs
@@ -6,14 +6,12 @@ namespace TechStore.Models
 
     public partial class ItemPedido
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
         public int Quantidade { get; set; }
 
-        [Column(TypeName = "decimal(8,2)")] // Define max 999.999,99
+        [Column(TypeName = "decimal(18,2)")]
         [Required]
         public decimal PrecoUnitario { get; set; }
         public int PedidoId { get; set; }
