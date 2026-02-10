74697c7be6b8e94c013ffde6738a463425fbbd21
ana <114084376+ana-borowsky@users.noreply.github.com>
2026-02-10 13:16:09 -0300

    [ajuste][pedido] remove código que não é utilizado

--- TechStore/Models/Pedido.cs
+++ TechStore/Models/Pedido.cs
@@ -6,8 +6,6 @@ namespace TechStore.Models
 {
     public partial class Pedido
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
