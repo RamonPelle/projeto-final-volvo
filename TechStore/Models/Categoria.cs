4b853f4622b097f8c67fc2dbd9d98581e3e63e59
ana <114084376+ana-borowsky@users.noreply.github.com>
2026-02-10 07:42:26 -0300

    [ajuste][categoria] remove código desnecessário

--- TechStore/Models/Categoria.cs
+++ TechStore/Models/Categoria.cs
@@ -7,8 +7,6 @@ namespace TechStore.Models
 
     public partial class Categoria
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
