606d2dfb06d78eb299c21115468e117584864a87
ana <114084376+ana-borowsky@users.noreply.github.com>
2026-02-11 11:17:28 -0300

    [ajuste][produto] remove [key]

--- TechStore/Models/Produto.cs
+++ TechStore/Models/Produto.cs
@@ -6,8 +6,6 @@ namespace TechStore.Models
 
     public partial class Produto
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
